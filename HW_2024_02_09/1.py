d04ddddbdd90e0d9f517af087fab26aef1450854
Alexey <bogwowwww@mail.ru>
2024-02-16 01:57:41 +0300

    сделал дз

---
+++ HW_2024_02_09/1.py
@@ -0,0 +1,23 @@
+number1 = float(input('\nвведите первое число '))
+number2 = float(input('\nвведите второе число '))
+number3 = float(input('\nвведите третье число '))
+
+sum = 0
+
+if number1 > 0:
+    sum += number1
+if number2 > 0:
+    sum += number2
+if number3 > 0:
+    sum += number3
+
+print(f'\nсумма положительных чисел равна {sum}')
+
+
+#введите первое число 9
+
+#введите второе число -2
+
+#введите третье число -3
+
+#сумма положительных чисел равна 9
